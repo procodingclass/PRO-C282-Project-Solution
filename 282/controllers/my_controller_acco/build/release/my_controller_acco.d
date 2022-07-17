ea32085c45767317662189543a155000700dd712
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-07-17 23:55:18 +0530

    Delete 282 directory

--- 282/controllers/my_controller_acco/build/release/my_controller_acco.d
+++
@@ -1,7 +0,0 @@
-build/release/my_controller_acco.o: my_controller_acco.c \
- C:\Program\ Files\Webots/include/controller/c/webots/accelerometer.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/led.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
